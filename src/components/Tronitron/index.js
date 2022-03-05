3f7b97a8b293bee3d2e0192fb06bf4df5470c828
ahza64 <justinmichaelpettit@gmail.com>
2022-03-05 11:58:02 -0800

    3d self model. build and download model. install dependecies, import components, laid out canvas, imported model, set options and completed 3d model on jumbotron first draft. intial bugs and crashes on page reload, but seems fixed now. will need extensive testing.

--- src/components/Tronitron/index.js
+++ src/components/Tronitron/index.js
@@ -1,6 +1,10 @@
-import { Button } from 'react-bootstrap'
-import { Container } from 'react-bootstrap'
-import './Tronitron.css'
+import { Button, Container } from 'react-bootstrap';
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Model from './3dselfmodel';
+import './Tronitron.css';
+
 
 export default function Tronitron() {
 
@@ -11,6 +15,22 @@ export default function Tronitron() {
           <h1>Justin Pettit</h1>
         </div>
         <h3>Software Engineer and Full Stack Web Developer</h3>
+        <Canvas
+          camera={{ position: [2, 0, 12.25], fov: 10 }}
+          style={{
+            backgroundColor: '#111a21',
+            width: '100vw',
+            height: '100vh',
+          }}
+          >
+          <ambientLight intensity={1.25} />
+          <ambientLight intensity={0.1} />
+          <directionalLight intensity={0.4} />
+          <Suspense fallback={null}>
+            <Model position={[0.025, -0.9, 0]} />
+          </Suspense>
+          <OrbitControls enableZoom={false}/>
+        </Canvas>
         <div>
           <p>Specializing in React and node.js. Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ornare justo eget quam sagittis, pulvinar finibus quam dignissim. Vestibulum ullamcorper cursus ligula, a luctus felis tempus ut. Suspendisse imperdiet ligula tincidunt nunc ullamcorper vestibulum. Duis porttitor sem a fermentum dapibus. Aliquam egestas tellus vitae faucibus lacinia. Aenean facilisis congue dolor.</p>
         </div>
