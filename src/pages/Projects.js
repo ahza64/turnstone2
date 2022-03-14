2791d8c17233fb62df75ce9d7bdee1bb05efa40b
ahza64 <justinmichaelpettit@gmail.com>
2022-03-14 13:37:28 -0700

    minor content and styling edits

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -10,22 +10,35 @@ function ArtAPI() {
   return (
     <div className="under-construction">
       <h1>
-        Art API Project Is Under Construction
+        Art API Project Is Under Construction, see above links for earlier renditions of skills
       </h1>
     </div>
   )
 }
 
 function ProjectPageContent(props) {
+  const imgPath = props.image
+  const styles = {
+    heroBackground: {
+      backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.44), rgba(0, 0, 0, 0.9)), url(${imgPath})`,
+      minHeight: "200px",
+      minWidth: "100vw",
+      backgroundSize: "cover",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    }
+  }
+
   return (
     <div className="project-content">
-      <div>
-        <img className="project-hero" src={props.image} alt=""/>
-      </div>
-      <div>
-        <h4>
-          {props.title}
-        </h4>
+      <div style={styles.heroBackground}>
+        <div>
+          <h4>
+            {props.title}
+          </h4>
+        </div>
       </div>
       <div>
         <p>
@@ -44,8 +57,8 @@ function ProjectPageContent(props) {
 function Reactor() {
   return (
     <ProjectPageContent
-      title="Reactor App"
-      image={reactor}
+      title="Reactor App (hooks only)"
+      image="https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
       description="using custom react hooks and pubsub, post blogs and give emoji reactions all tagged with your user name."
       href="https://vibrant-darwin-54679b.netlify.app/"
     />
@@ -55,8 +68,8 @@ function Reactor() {
 function DataViz() {
   return (
     <ProjectPageContent
-      title="DataViz"
-      image={lion}
+      title="DataViz (interm with home app external APIs)"
+      image="https://images.unsplash.com/photo-1542903660-eedba2cda473?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
       description="actually a home page app for the time being, several react custom hooks component features divided by line breaks. dynamic data feed controls. etc..."
       href="https://relaxed-hypatia-11c312.netlify.app/"
     />
